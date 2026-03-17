8c836879636d63479d0b5704843f93a91b30e7f9
naveen kumar <naveensddp1@gmail.com>
2026-03-17 15:35:09 +0530

    8th commit

--- deployment/eks-terraform/main.tf
+++ deployment/eks-terraform/main.tf
@@ -29,7 +29,8 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
-
+  enable_cluster_creator_admin_permissions = true
+  
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]
   }
