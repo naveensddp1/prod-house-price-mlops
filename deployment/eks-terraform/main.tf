e2b0ba4de47712494ecead70fa6848ac16decbd4
naveen kumar <naveensddp1@gmail.com>
2026-03-17 15:05:38 +0530

    7th commit

--- deployment/eks-terraform/main.tf
+++ deployment/eks-terraform/main.tf
@@ -1,33 +1,49 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  name    = "eks-vpc"
-  cidr    = "10.0.0.0/16"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
-
-
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.medium"]
+  }
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.medium"]
-      desired_size   = 2
-      min_size       = 1
-      max_size       = 3
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
