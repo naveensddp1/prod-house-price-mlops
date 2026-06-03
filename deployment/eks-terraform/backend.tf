ee6b4a955be8d6720ada63d03180c283ddd1df2d
naveensddp1 <163538362+naveensddp1@users.noreply.github.com>
2026-06-03 17:00:15 +0530

    Update S3 key for Terraform backend configuration [skip ci]

--- deployment/eks-terraform/backend.tf
+++ deployment/eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "naveen-eks-tf-state"
-    key            = "eks/terraform-useast1.tfstate"
+    key            = "us-east-1/terraform-useast1.tfstate"
     region         = "ap-south-1"
     dynamodb_table  = "terraform-state-lock-table"
   }
